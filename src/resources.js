4db360eafb5944d793c9dff449b0bfb7dbf722f3
SmartNi <nijun717@gmail.com>
2018-03-21 10:58:00 +0800

    fix

--- src/resources.js
+++ src/resources.js
@@ -2,8 +2,8 @@ const debug = process.env.NODE_ENV === 'development' ? true : false;
 // 测试环境地址
 if (debug) {
     // var host = 'https://api.guangyangyundong.com/api';
-    var host = 'http://127.0.0.1:8080/api';
-    // var host = 'http://119.23.12.36:8081/wzsport-server';
+    // var host = 'http://127.0.0.1:8080/api';
+    var host = 'http://119.23.12.36:8081/wzsport-server';
     //   var host = 'http://192.168.1.107:8080/api';
 } else {
     //正式环境地址
